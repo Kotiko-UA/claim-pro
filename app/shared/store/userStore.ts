10509767e67d90a3cece83ec49dee4b88be7b04d
kotiko <megasuslik666@gmail.com>
2025-10-08 11:57:53 +0300

    fix

--- app/shared/store/userStore.ts
+++ app/shared/store/userStore.ts
@@ -1,24 +1,20 @@
 import { defineStore } from 'pinia'
 import type { UserType } from '@/shared/types/user-type'
 
-export const useUserStore = defineStore(
-  'user',
-  () => {
-    const user = reactive<UserType>({
-      firstName: '',
-      lastName: '',
-      email: '',
-      verified: false,
-      type: '',
-    })
-    const changeUser = (data: UserType) => {
-      data.firstName && (user.firstName = data.firstName)
-      data.lastName && (user.lastName = data.lastName)
-      data.email && (user.email = data.email)
-      data.verified && (user.verified = data.verified)
-      data.type && (user.type = data.type)
-    }
-    return { user, changeUser }
-  },
-  { persist: true }
-)
+export const useUserStore = defineStore('user', () => {
+  const user = reactive<UserType>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    verified: false,
+    type: '',
+  })
+  const changeUser = (data: UserType) => {
+    data.firstName && (user.firstName = data.firstName)
+    data.lastName && (user.lastName = data.lastName)
+    data.email && (user.email = data.email)
+    data.verified && (user.verified = data.verified)
+    data.type && (user.type = data.type)
+  }
+  return { user, changeUser }
+})
